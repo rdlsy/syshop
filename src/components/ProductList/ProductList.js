f2b7a60627d77c1721ab2a44c1c5e60192a6e53d
lsy <lsy_65@naver.com>
2021-02-12 12:50:47 +0900

    가격 수정

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import { List, Item, Img, Name, Price } from './style';
 
 export default function ProductList({ products }) {
+  const setPrice = (num) => {
+    let price = num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return price;
+  }
+
   return (
     <List>
       {products.map(product => (
@@ -10,7 +16,7 @@ export default function ProductList({ products }) {
           <Link to={`/product/${product.id}`}>
             <Img><img src={product.imgSrc} alt={product.name} /></Img>
             <Name>{product.name}</Name>
-            <Price>판매가 : <b>{product.price}</b>원</Price>
+            <Price>판매가 : <b>{setPrice(product.price)}</b>원</Price>
           </Link>
         </Item>
       ))}
